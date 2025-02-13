56f351a11d14e2abb69e4a50cf39d93c3ea473b9
Joseph West <joe@incitenewbusiness.co.uk>
2025-02-13 09:10:32 +0000

    New hover block, and hero fixes

--- page-templates/blocks/cb_hover_boxes_detail.php
+++ page-templates/blocks/cb_hover_boxes_detail.php
@@ -6,7 +6,7 @@ if ($columns):
 ?>
 
 <section class="boxes" style="position: relative;">
-    <div class="container py-5">
+    <div class="container py-2">
         <div class="row">
             <div class="col-12 mx-auto pb-4">
                 <h2><?php echo $heading; ?></h2>
